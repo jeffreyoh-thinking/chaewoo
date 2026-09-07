898b3bccde028c6b72195dc8a4d1980567105318
jeffreyoh-thinking <sgtohks@gmail.com>
2026-09-07 14:21:52 +0900

    Add files via upload

--- emailjs-config.js
+++ emailjs-config.js
@@ -3,7 +3,7 @@
 // Email Templates > 해당 템플릿 > "To Email" 항목에 입력해야 합니다. (보안상 클라이언트 코드에는 넣지 않음)
 const EMAILJS_PUBLIC_KEY = "DzMiI81kMuJWxsI5j";
 const EMAILJS_SERVICE_ID = "service_mux59h6";
-const EMAILJS_TEMPLATE_ID = "DzMiI81kMuJWxsI5j";
+const EMAILJS_TEMPLATE_ID = "template_f0yk8ef";
 
 if (typeof emailjs !== "undefined" && EMAILJS_PUBLIC_KEY !== "YOUR_PUBLIC_KEY") {
   emailjs.init({ publicKey: EMAILJS_PUBLIC_KEY });
